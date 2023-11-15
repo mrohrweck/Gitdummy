7d10806fb15d3cadee6f62567e7dd5307cd6df39
mrohrweck <monika.rohrweck@htl-steyr.ac.at>
2023-11-15 18:24:26 +0100

    artefact

---
+++ src/main/java/com/example/demo/Launcher.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class Launcher {
+
+    /**
+     * Click the green "play" - icon to run the applictaion.
+     * @param args
+     */
+    public static void main(String[] args) {
+        HelloApplication.main(args);
+    }
+
+}
